545e8b4e54951c0c66f459ba35e39d80c31938cd
Benjamin Golden <b.j.golden@outlook.com>
2021-05-27 11:58:50 -0500

    saved progress on phase 4

--- Program.cs
+++ Program.cs
@@ -11,7 +11,10 @@ namespace Quest
     {
         static void Main(string[] args)
         {
-            
+            Robe adventurerRobe = new Robe();
+            adventurerRobe.Length = 12;
+            adventurerRobe.Colors.Add("blue");
+
             Console.Write("Hi! What's your name? ");
             string name = Console.ReadLine();
             
@@ -49,7 +52,7 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer(name);
+            Adventurer theAdventurer = new Adventurer(name, adventurerRobe);
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
